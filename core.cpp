51adc541f83ecbe483d065103a2281360c672320
contiliosung <sungchenhsi@gmail.com>
2019-11-10 23:48:25 +0000

    -core.cpp: bug fixed/ func add, minor change

---
+++ core.cpp
@@ -0,0 +1,9 @@
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <pcl/common/centroid.h>
+#include <pcl/filters/extract_indices.h>
+#include <omp.h>
+#include "core.h"
+
+
+namespace po = boost::program_options;
