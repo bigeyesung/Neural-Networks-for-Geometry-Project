336946d9e1ebf6bd51eaab26adf22be3674dbdf3
contiliosung <sungchenhsi@gmail.com>
2019-11-10 23:48:41 +0000

    -core.cpp: bug fixed/ func add, minor change

--- core.cpp
+++ core.cpp
@@ -7,3 +7,12 @@
 
 
 namespace po = boost::program_options;
+
+// Processes command line arguments and returns the default values if the parameters are not specified by the user
+bool processCommandLine(int argc, char** argv,
+	std::string &file_cloud,
+	float &support_radius,
+	int &num_voxels,
+	float &smoothing_kernel_width,
+	std::string &file_keypoints,
+	std::string &output_folder)
