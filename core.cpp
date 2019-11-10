126a4b34eade2c693d97b57117e9555cde53ba37
contiliosung <sungchenhsi@gmail.com>
2019-11-10 23:48:56 +0000

    -core.cpp: bug fixed/ func add, minor change

--- core.cpp
+++ core.cpp
@@ -16,3 +16,22 @@ bool processCommandLine(int argc, char** argv,
 	float &smoothing_kernel_width,
 	std::string &file_keypoints,
 	std::string &output_folder)
+
+    {
+	try
+	{
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help", "Given a point cloud and a support radius, this code generates a SDV voxel representation for the selected interest points.")
+			("fileCloud,f", po::value<std::string>(&file_cloud)->required(), "Input point cloud file in .ply format")
+			("supportRadius,r", po::value<float>(&support_radius)->default_value(0.150),
+				"Half size of the voxel grid.")
+			("numVoxels,n", po::value<int>(&num_voxels)->default_value(16),
+				"Number of voxels in a side of the grid. Whole grid is nxnxn.")
+			("smoothingKernelWidth,h", po::value<float>(&smoothing_kernel_width)->default_value(1.75),
+				"Width of the Gaussia kernel used for smoothing.")
+			("fileKeypoints,k", po::value<std::string>(&file_keypoints)->default_value("0"),
+				"Path to the file with the indices of the interest points. If 0, SDV voxel grid representation if computed for all the points")
+			("outputFolder,o", po::value<std::string>(&output_folder)->default_value("./data/sdv_voxel_grid/"),
+				"Output folder path.")
+			;
