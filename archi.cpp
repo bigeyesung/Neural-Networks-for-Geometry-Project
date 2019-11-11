0ecb4c2190175bbdc44750b0d10234f649e51e14
contiliosung <sungchenhsi@gmail.com>
2019-11-11 23:52:09 +0000

    -archi.cpp: bug fixed/ func add, minor change

--- archi.cpp
+++ archi.cpp
@@ -11,3 +11,11 @@ def network_architecture(x_anc,x_pos, dropout_rate, config, reuse=False):
     net_structure = [1, 32, 32, 64, 64, 128, 128]
     outputDim = config.output_dim
     channels = [item for sublist in [net_structure, [outputDim]] for item in sublist]
+
+    # In the third layer stride is 2
+    stride = np.ones(len(channels))
+    stride[2] = 2
+
+    # Apply dropout in the 6th layer
+    dropout_flag = np.zeros(len(channels))
+    dropout_flag[5] = 1
