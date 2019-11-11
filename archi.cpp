7d91734b1e37dba52107948dcb480d8749ac61dc
contiliosung <sungchenhsi@gmail.com>
2019-11-11 23:51:52 +0000

    -archi.cpp: bug fixed/ func add, minor change

--- archi.cpp
+++ archi.cpp
@@ -4,3 +4,10 @@ import numpy as np
 
 # Import custom functions
 from core import ops
+
+def network_architecture(x_anc,x_pos, dropout_rate, config, reuse=False):
+
+    # Join the 3DSmoothNet structure with the desired output dimension
+    net_structure = [1, 32, 32, 64, 64, 128, 128]
+    outputDim = config.output_dim
+    channels = [item for sublist in [net_structure, [outputDim]] for item in sublist]
