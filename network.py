33a15d2b1f8c96672554e2a59c3c2862cdf47a60
contiliosung <sungchenhsi@gmail.com>
2019-11-12 23:56:11 +0000

    -network.py: bug fixed/ func add, minor change

--- network.py
+++ network.py
@@ -7,3 +7,13 @@ import datetime
 from core import ops
 
 from tqdm import trange
+
+class NetworkBuilder(object):
+    """Network builder class """
+
+    def __init__(self, config):
+
+        self.config = config
+
+        # Initialize tensorflow session
+        self._init_tensorflow()
