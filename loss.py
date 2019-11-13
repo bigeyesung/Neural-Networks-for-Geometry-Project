4d513db636262ec42d08d1b595b32395a5bbebb7
contiliosung <sungchenhsi@gmail.com>
2019-11-13 23:58:50 +0000

    -loss.py: bug fixed/ func add, minor change

--- loss.py
+++ loss.py
@@ -15,3 +15,21 @@ def all_diffs(a, b):
         mean is used.
     """
     return tf.expand_dims(a, axis=1) - tf.expand_dims(b, axis=0)
+
+def cdist(a, b, metric='euclidean'):
+    """Similar to scipy.spatial's cdist, but symbolic.
+    The currently supported metrics can be listed as `cdist.supported_metrics` and are:
+        - 'euclidean', although with a fudge-factor epsilon.
+        - 'sqeuclidean', the squared euclidean.
+        - 'cityblock', the manhattan or L1 distance.
+    Args:
+        a (2D tensor): The left-hand side, shaped (B1, F).
+        b (2D tensor): The right-hand side, shaped (B2, F).
+        metric (string): Which distance metric to use, see notes.
+    Returns:
+        The matrix of all pairwise distances between all vectors in `a` and in
+        `b`, will be of shape (B1, B2).
+    Note:
+        When a square root is taken (such as in the Euclidean case), a small
+        epsilon is added because the gradient of the square-root at zero is
+        undefined. Thus, it will never return exact zero in these cases.
