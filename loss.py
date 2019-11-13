b14edbba74f2be868f05672fda0feaad22393c51
contiliosung <sungchenhsi@gmail.com>
2019-11-13 23:58:30 +0000

    -loss.py: bug fixed/ func add, minor change

--- loss.py
+++ loss.py
@@ -1,3 +1,17 @@
 import numbers
 import numpy as np
 import tensorflow as tf
+
+def all_diffs(a, b):
+    """ Returns a tensor of all combinations of a - b.
+    Args:
+        a (2D tensor): A batch of vectors shaped (B1, F).
+        b (2D tensor): A batch of vectors shaped (B2, F).
+    Returns:
+        The matrix of all pairwise differences between all vectors in `a` and in
+        `b`, will be of shape (B1, B2).
+    Note:
+        For convenience, if either `a` or `b` is a `Distribution` object, its
+        mean is used.
+    """
+    return tf.expand_dims(a, axis=1) - tf.expand_dims(b, axis=0)
