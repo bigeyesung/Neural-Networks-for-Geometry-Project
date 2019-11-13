62a361a216358bd2109fc7e6711b9b75662d0e95
contiliosung <sungchenhsi@gmail.com>
2019-11-13 23:57:22 +0000

    -logger.py: bug fixed/ func add, minor change

--- logger.py
+++ logger.py
@@ -1,3 +1,9 @@
 
 import logging
 # import auxiliary_module
+
+def loggerGenerator(name=None, level=logging.DEBUG, fileName='log.log'):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # create logger with 'spam_application'
+    logger = logging.getLogger(name=name)
+    logger.setLevel(level=level)
