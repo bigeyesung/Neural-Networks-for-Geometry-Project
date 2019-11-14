f55db1a5b3efdede2f74d2d9c4505e292fd344ae
contiliosung <sungchenhsi@gmail.com>
2019-11-14 23:57:29 +0000

    -ops.py: bug fixed/ func add, minor change

--- ops.py
+++ ops.py
@@ -9,3 +9,8 @@ def weight(shape, layer_name, weight_initializer=None,reuse=False):
         weight_initializer = tf.orthogonal_initializer(gain=0.6,
                                                        seed=41,
                                                        dtype=tf.float32)
+
+                                                           with tf.name_scope(layer_name):
+        with tf.variable_scope(layer_name, reuse=reuse):
+            weights = tf.get_variable(layer_name + "_W", shape=shape,
+                                      dtype=tf.float32, initializer=weight_initializer)
