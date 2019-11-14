1eb0e662ed1346cb643aa180153aabf2e8eec604
contiliosung <sungchenhsi@gmail.com>
2019-11-14 23:57:18 +0000

    -ops.py: bug fixed/ func add, minor change

--- ops.py
+++ ops.py
@@ -2,3 +2,10 @@
 import tensorflow as tf
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+
+# Create lots of weights and biases & Initialize with a small positive number as we will use ReLU
+def weight(shape, layer_name, weight_initializer=None,reuse=False):
+    if weight_initializer is None:
+        weight_initializer = tf.orthogonal_initializer(gain=0.6,
+                                                       seed=41,
+                                                       dtype=tf.float32)
