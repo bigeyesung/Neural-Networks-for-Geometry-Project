295b9b033c86322b7e5cc5cd77a7e5143dafee8e
contiliosung <sungchenhsi@gmail.com>
2019-11-15 00:00:24 +0000

    -ops.py: bug fixed/ func add, minor change

--- ops.py
+++ ops.py
@@ -83,3 +83,9 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
         if shuffle:
             shuffle_indices = np.random.permutation(np.arange(data_size))
             shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
