0ddd1a6a045708843a46f11d4511938ced784de1
contiliosung <sungchenhsi@gmail.com>
2019-11-15 23:58:03 +0000

    -saveDataToTFrecordsExample.py: bug fixed/ func add, minor change

--- saveDataToTFrecordsExample.py
+++ saveDataToTFrecordsExample.py
@@ -10,3 +10,10 @@ import tensorflow as tf
 def npy_to_tfrecords(features,pairs,output_file):
     # write records to a tfrecords file
     writer = tf.python_io.TFRecordWriter(output_file)
+
+    # Loop through all the features you want to write
+    for index in range(0, pairs.shape[0]):
+        # Feature contains a map of string to feature proto objects
+        feature = {}
+        feature['X'] = tf.train.Feature(float_list=tf.train.FloatList(value=features[pairs[index,0],:]))
+        feature['Y'] = tf.train.Feature(float_list=tf.train.FloatList(value=features[pairs[index,1],:]))
