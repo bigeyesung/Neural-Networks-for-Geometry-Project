97a687300da2e6f85b382c29a73a4e846698f819
contiliosung <sungchenhsi@gmail.com>
2019-11-15 23:57:54 +0000

    -saveDataToTFrecordsExample.py: bug fixed/ func add, minor change

--- saveDataToTFrecordsExample.py
+++ saveDataToTFrecordsExample.py
@@ -6,3 +6,7 @@ from os.path import isfile, join, sep
 import subprocess
 from glob import glob
 import tensorflow as tf
+
+def npy_to_tfrecords(features,pairs,output_file):
+    # write records to a tfrecords file
+    writer = tf.python_io.TFRecordWriter(output_file)
