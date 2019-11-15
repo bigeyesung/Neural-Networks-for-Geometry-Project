175efcffea7ebef3bce831c56cd58bcbc622e638
contiliosung <sungchenhsi@gmail.com>
2019-11-15 23:55:02 +0000

    -config.py: bug fixed/ func add, minor change

---
+++ config.py
@@ -0,0 +1,12 @@
+# Import python dependencies
+import argparse
+
+
+arg_lists = []
+parser = argparse.ArgumentParser()
+
+
+def add_argument_group(name):
+    arg = parser.add_argument_group(name)
+    arg_lists.append(arg)
+    return arg
